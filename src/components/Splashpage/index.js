3f564935cd9f7c812113bdf737eb86e0d7a5a5d5
Drewthurm21 <drewthurm21@gmail.com>
2022-02-23 18:02:33 -0600

    updated splash page specs

--- src/components/Splashpage/index.js
+++ src/components/Splashpage/index.js
@@ -3,10 +3,13 @@
   Create the Splashpage component and return it
 */
 
-
 /*    SPLASHPAGE COMPONENT
-  Create a simple splash page that displays displays a pokemon game cover, centered on the page.
+  Create a simple splash page that displays a pokemon game cover
+    it should be 800px tall & centered on the page.
     the game cover should be a link to the homepage
+
+    coverURL:
+    'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpre07.deviantart.net%2F3278%2Fth%2Fpre%2Fi%2F2016%2F060%2F1%2Fc%2Fpokemon_yellow_version__game_boy__hq_box_art_by_jadelune-d9tkrb7.png&f=1&nofb=1',
 */
 
 const SplashpageContainer = () => {
